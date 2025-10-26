89a7fd032cca5d55b1bceea7cd85b8d43abc8729
Ridwanullah <aladebaruwa@gmail.com>
2025-10-26 13:06:59 +0100

    Implement functionalities like coins collection(bonus and daily farming) pop-up notifications for leaf collection and enhance styling

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
 let actualBalance = document.getElementById("actual-balance");
 let bonus = document.getElementById("get-bonus");
+let farmCoins = document.getElementById("farm-coins");
+const popUp = document.getElementById("pop-up");
 
-let actualBalanceInt = parseInt(actualBalance);
-
+let balance = 0;
+let actualBalanceInt = parseInt(balance);
+const dailyBonus = 50;
 
 
 bonus.addEventListener("click", () => {
     console.log(actualBalanceInt);
-    actualBalanceInt += 50;
-    actualBalance.textContent = actualBalanceInt;
+
+    const bonusDeterminer = checkClicks();
+    console.log(bonusDeterminer);
+
+    if (bonusDeterminer) {
+
+        actualBalanceInt += dailyBonus;
+        actualBalance.textContent = actualBalanceInt;
+
+        popUp.innerHTML = `<p>You got ${dailyBonus} leaves!</p>`;
+
+        popUp.classList.add("active");
+        setTimeout(function () {
+            popUp.classList.remove("active");
+        }, 3000);
+
+    } else {
+        popUp.innerHTML = `<p>You've already claimed the day bonus leaf(s).</p>`
+
+        popUp.classList.add("active");
+        setTimeout(function () {
+            popUp.classList.remove("active");
+        }, 3000)
+    }
 });
+
+farmCoins.addEventListener("click", () => {
+    let randomNumber = Math.floor(Math.random() * 20);
+    farmCoins.textContent = `You got ${randomNumber} leaves.`;
+    actualBalanceInt += randomNumber;
+    actualBalance.textContent = actualBalanceInt;
+
+    popUp.innerHTML = `<p>You got ${actualBalanceInt} leaves.</p>`
+
+    popUp.classList.add("active");
+    setTimeout(function () {
+        popUp.classList.remove("active");
+    }, 3000)
+})
+
+const lastClickDateKey = 'lastButtonClickDate';
+
+function checkClicks() {
+    const lastClickDate = localStorage.getItem(lastClickDateKey);
+
+    if (!lastClickDate) {
+        return true
+    }
+
+    const today = new Date();
+    const storedDate = new Date(lastClickDate);
+
+    const comparedYear = today.getFullYear() !== storedDate.getFullYear();
+    const comparedMonth = today.getMonth() !== storedDate.getMonth();
+    const comparedDay = today.getDay() !== storedDate.getDate();
+
+    return comparedYear || comparedMonth || comparedDay;
+}
